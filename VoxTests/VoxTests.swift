2e240c8b6f0e8b2eadaf51581e7d5f2bd1af05ef
Paulo Henrique dos Santos <paulo.santos@poatek.com>
2019-05-04 10:53:43 -0300

    Initial Commit

---
+++ VoxTests/VoxTests.swift
@@ -0,0 +1,34 @@
+//
+//  VoxTests.swift
+//  VoxTests
+//
+//  Created by Paulo Henrique dos Santos on 04/05/19.
+//  Copyright © 2019 Paulo Henrique dos Santos. All rights reserved.
+//
+
+import XCTest
+@testable import Vox
+
+class VoxTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
